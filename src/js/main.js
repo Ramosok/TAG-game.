55d73cf717a27aa9638a2ac9aa108d89206ca7b2
Ramosok <Ramosok86@gmail.com>
2021-08-24 06:25:26 +0300

    feat: add shuffle button, add Favicon.

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,22 @@ const field = document.querySelector('.field');
 const change = document.getElementsByClassName('change');
 const tour = document.querySelector('#tour');
 const winModal = document.querySelector('#winModal');
+const mixItUpBtn = document.querySelector('#mixItUpBtn');
 
+mixItUpBtn.addEventListener('click',()=> {
+  window.location.reload(true);
+});
 
-let arr = [15,16,14,13,12,11,10,9,8,7,6,5,4,3,2,1];
+function vvv() {
+  winModal.classList.remove('onActive');
+  shuffle()
+}
+
+const arr = [];
+for (let i = 1; i <= 16; i++) {
+  arr.push(i);
+}
+
 function shuffle() {
   for (let i = arr.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * (i + 1));
@@ -13,28 +26,31 @@ function shuffle() {
     arr[j] = temp;
   }
 }
+
 shuffle()
-  function renderItems() {
-    arr.forEach(item => {
-      const container = document.createElement('div');
-      container.classList.add('active');
-      container.setAttribute('id', `elem${item}`);
-      container.innerText = `${item}`
-      field.append(container);
-      if (+container.innerText === 16) {
-        container.classList.remove('active');
-        container.innerText = '';
-      }
-    })
+
+function renderItems() {
+  arr.forEach(item => {
+    const container = document.createElement('div');
+    container.classList.add('active');
+    container.setAttribute('id', `elem${item}`);
+    container.innerText = `${item}`
+    field.append(container);
+    if (+container.innerText === 16) {
+      container.classList.remove('active');
+      container.innerText = '';
+    }
+  })
 }
+
 renderItems()
 const vArr = '15 14 13 12 11 10 9 8 7 6 5 4 3 2 1'
-function victory () {
+
+function victory() {
   const active = document.querySelectorAll('.active');
-  let arrInnerHtml = [active[0].innerHTML,active[1].innerHTML,active[2].innerHTML,active[3].innerHTML,active[4].innerHTML,active[5].
-    innerHTML,active[6].innerHTML,active[7].innerHTML,active[8].innerHTML,active[9].innerHTML,active[10].innerHTML,
-    active[11].innerHTML,active[12].innerHTML,active[13].innerHTML,active[14].innerHTML,];
-  if(vArr === arrInnerHtml.join(' ')) {
+  let arrInnerHtml = [active[0].innerHTML, active[1].innerHTML, active[2].innerHTML, active[3].innerHTML, active[4].innerHTML, active[5].innerHTML, active[6].innerHTML, active[7].innerHTML, active[8].innerHTML, active[9].innerHTML, active[10].innerHTML,
+    active[11].innerHTML, active[12].innerHTML, active[13].innerHTML, active[14].innerHTML,];
+  if (vArr === arrInnerHtml.join(' ')) {
     console.log('Victory')
     winModal.classList.remove('onActive');
   }
@@ -52,7 +68,7 @@ function isMovvingAvailable(itemToMove, targetElem) {
   return false;
 }
 
-field.addEventListener('click',({target}) => {
+field.addEventListener('click', ({target}) => {
   if (target === field) return;
   let itemChange = change[0];
   if (!itemChange) {
